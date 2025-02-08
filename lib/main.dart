a83d087ee1ccd7e5b6c642b005088ed2c8d5aae4
Reemaalnafisa <140368661+Reemaalnafisa@users.noreply.github.com>
2025-02-08 16:58:00 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:gat_helper_app/features/auth/views/sign_up_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gat Helper',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SignUpPage(),
+    );
+  }
+}
+
