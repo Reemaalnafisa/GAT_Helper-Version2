3c8d740ae26a07fddaad556b66a09e861d7106e1
Nada <nadazomansaad@gmail.com>
2025-02-27 06:18:45 +0300

    add student home page

--- lib/features/auth/views/student.dart
+++ lib/features/auth/views/student.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gat_helper_app/features/auth/views/AR_dashboard.dart';
 import 'package:gat_helper_app/features/auth/views/All_req_page.dart';
 import 'package:gat_helper_app/features/auth/views/GG_waiting.dart';
-import 'package:gat_helper_app/features/auth/views/Group_GameQ.dart';
 import 'package:gat_helper_app/features/auth/views/find_game_page.dart';
+import 'package:gat_helper_app/features/auth/views/self_game_config.dart';
 import 'package:gat_helper_app/features/common/start_page.dart';
 
 import '../../common/edit_profile_page.dart';
@@ -43,223 +43,226 @@ class StudentHome extends State<StudentHomePage> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        backgroundColor: Colors.white,
-        drawer: Drawer(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 200,
-                width: 500,
-                child: DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF284379),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const CircleAvatar(
-                        radius: 40,
-                        backgroundImage: AssetImage("assets/student.png"),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        "Huda Omer",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                    ],
-                  ),
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: 500,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xFF284379),
                 ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.edit, color: Colors.black),
-                title: const Text('Edit your profile'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EditProfilePage(role: 'student', initialData: {}),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage("assets/student.png"),
                     ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.messenger_outline, color: Colors.black),
-                title: const Text('Requests'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const RequestsPage(),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Huda Omer",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
                     ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.contact_mail, color: Colors.black),
-                title: const Text('Contact Us'),
-                onTap: () {
-                  _showContactUsDialog(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.logout, color: Colors.red),
-                title: const Text('Log Out'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const StartPage(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Stack(
-          children: [
-          Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Image.asset(
-            "assets/blue_background.png",
-            fit: BoxFit.cover,
-            height: 380,
-            width: screenWidth,
-          ),
-        ),
-        Positioned(
-          top: 20,
-          left: 10,
-          child: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: const Icon(Icons.menu, color: Colors.black, size: 30),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              );
-            },
-          ),
-        ),
-            Positioned(
-              top: 10,
-              right: 5,
-              child: CircleAvatar(
-                radius: 30,
-                backgroundImage: AssetImage("assets/student.png"),
-              ),
-            ),
-
-
-
-
-
-
-        Column(
-            children: [
-            _buildProfileHeader(),
-              Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 80),
-                      const Text(
-                        "Select Your Game",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 8), // Spacing between text and images
-                      // Row with two images next to each other
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => GroupGamePageWidget()),
-                              );
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(90),
-                              child: Ink.image(
-                                image: AssetImage('assets/group_game.png'),
-                                height: 210,
-                                width: 170,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                                onTap: () {
-                                  builder: (context) => LeaderWidget();
-                                },
-                              child: ClipRRect(
-                              borderRadius: BorderRadius.circular(90), // تغيير الرقم لضبط مقدار الانحناء
-                              child: Ink.image(
-                                image: AssetImage('assets/self_game.png'),
-                                height: 210,
-                                width: 170,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [const Text(
-                          "My Parents",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                                        ]
-                      ),
-                    ),
-                      Row(
-
-                        children: parents
-                            .map((parent) => _buildParentCard(
-                          parent["name"]!,
-                          parent["email"]!,
-                          parent["status"]!,
-                          parent["gender"]!, // ✅ أضفنا `gender` هنا
-                              (newStatus) {
-                            setState(() {
-                              if (newStatus == "Rejected") {
-                                parents.removeWhere((p) => p["name"] == parent["name"]);
-                              } else {
-                                parent["status"] = newStatus;
-                              }
-                            });
-                          },
-                        ))
-
-                            .toList(),
-                      ),
-
-                    ],
-                  ),
+                  ],
                 ),
               ),
             ),
-
-        ]
-        ),
+            ListTile(
+              leading: const Icon(Icons.edit, color: Colors.black),
+              title: const Text('Edit your profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditProfilePage(role: 'student', initialData: {}),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.messenger_outline, color: Colors.black),
+              title: const Text('Requests'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RequestsPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.contact_mail, color: Colors.black),
+              title: const Text('Contact Us'),
+              onTap: () {
+                _showContactUsDialog(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout, color: Colors.red),
+              title: const Text('Log Out'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const StartPage(),
+                  ),
+                );
+              },
+            ),
           ],
         ),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/blue_background.png",
+              fit: BoxFit.cover,
+              height: 380,
+              width: screenWidth,
+            ),
+          ),
+          Positioned(
+            top: 20,
+            left: 10,
+            child: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+          ),
+          Positioned(
+            top: 30,
+            right: 12,
+            child: CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage("assets/student.png"),
+            ),
+          ),
+
+
+
+
+
+
+          Column(
+              children: [
+                _buildProfileHeader(),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 50),
+                          const Text(
+                            "Select Your Game",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8), // Spacing between text and images
+                          // Row with two images next to each other
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => GroupGamePage2()),
+                                  );
+                                },
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(90),
+                                  child: Ink.image(
+                                    image: AssetImage('assets/img_27.png'),
+                                    height: 230,
+                                    width: 170,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => SelfgameconfigWidget()),
+                                  );
+                                },
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(90), // تغيير الرقم لضبط مقدار الانحناء
+                                  child: Ink.image(
+                                    image: AssetImage('assets/img_26.png'),
+                                    height: 230,
+                                    width: 170,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                                children: [const Text(
+                                  "My Parents",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                ]
+                            ),
+                          ),
+                          Row(
+
+                            children: parents
+                                .map((parent) => _buildParentCard(
+                              parent["name"]!,
+                              parent["email"]!,
+                              parent["status"]!,
+                              parent["gender"]!, // ✅ أضفنا gender هنا
+                                  (newStatus) {
+                                setState(() {
+                                  if (newStatus == "Rejected") {
+                                    parents.removeWhere((p) => p["name"] == parent["name"]);
+                                  } else {
+                                    parent["status"] = newStatus;
+                                  }
+                                });
+                              },
+                            ))
+
+                                .toList(),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+              ]
+          ),
+        ],
+      ),
     );
 
   }
@@ -316,9 +319,9 @@ class StudentHome extends State<StudentHomePage> {
 
 
 
-    Column(
+          Column(
             children: [
-              const SizedBox(height: 50),
+              const SizedBox(height: 80),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -334,7 +337,7 @@ class StudentHome extends State<StudentHomePage> {
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Dashboard()), // استبدل `NewPage` بصفحتك
+                        MaterialPageRoute(builder: (context) => Dashboard()), // استبدل NewPage بصفحتك
                       );
                     },
                   ),
@@ -358,7 +361,7 @@ class StudentHome extends State<StudentHomePage> {
         width: 350,  // زيادة العرض
         height: 170, // زيادة الارتفاع لاستيعاب التاريخ
         decoration: BoxDecoration(
-          color: Colors.orange.shade200,
+          color: Colors.orange.shade200.withOpacity(0.5),
           borderRadius: BorderRadius.circular(20),
           boxShadow: [
             BoxShadow(
@@ -544,7 +547,7 @@ class StudentHome extends State<StudentHomePage> {
   Widget _buildParentCard(
       String name, String email, String status, String gender, Function(String) onStatusChanged) {
     return Container(
-      margin: EdgeInsets.only(top: 6, left: 10),
+      margin: EdgeInsets.only(top: 6, left: 15),
       width: 170,
       height: 120,
 
@@ -569,7 +572,7 @@ class StudentHome extends State<StudentHomePage> {
               radius: 20,
               backgroundColor: Colors.white70,
               backgroundImage: AssetImage(
-                gender == "male" ? "assets/parent1.png" : "assets/parent2.png",
+                gender == "male" ? "assets/parent1.png" : "assets/img_28.png",
               ),
             ),
           ),
@@ -603,7 +606,7 @@ class StudentHome extends State<StudentHomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(height: 60),
+                SizedBox(height: 50),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: Text(
@@ -665,4 +668,3 @@ class StudentHome extends State<StudentHomePage> {
   }
 
 }
-
